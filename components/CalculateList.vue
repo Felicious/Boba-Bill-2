00f472468918b8692b3d0387ac1ad9fd34522934
Felicious <freakyotaku@gmail.com>
2021-03-16 22:18:04 -0700

    add method for computing totals

--- components/CalculateList.vue
+++ components/CalculateList.vue
@@ -4,15 +4,44 @@
 export default {
   // variables accepted from parent
   props: {
-    friends: Array
+    friends: Array,
+    transactions: Array
   },
   data() {
     return {
       // local array whose index corresponds to friends
       // do not want to mutate prop o;
-      friendsOwed: []
+      friendsOwed: [
+        //name: "",
+        //owed: 0
+      ]
     };
   },
-  methods: {}
+  methods: {
+    /* 
+		id, name, expense, ppl:[], payers:[]  */
+    calculateOwed(transaction) {
+      // amount everyone owes if split evenly
+      let individCost = transaction.cost / transaction.ppl.length;
+      for (let i = 0; i < transaction.ppl.length; i++) {
+        const person = transaction.ppl[i];
+        const index = this.friends.indexOf(person);
+        // add to friendsOwed[index]
+
+        // is this person also a payer?
+
+        if (transaction.payer.includes(person)) {
+          // YES: expect a negative value
+          const payerPaid = transaction.cost / transaction.payer.length;
+          friendsOwed[index] += individCost - payerPaid;
+        } else {
+          // NO:
+          friendsOwed[index] += individCost;
+        }
+        // ALERT: what if a payer paid but didn't eat?
+        // TODO: handle this edge case
+      }
+    }
+  }
 };
 </script>
