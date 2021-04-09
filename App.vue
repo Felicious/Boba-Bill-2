a292f01c719d4b65421776837b250fd63c0ad175
Felicious <freakyotaku@gmail.com>
2021-04-08 18:23:57 -0700

    Begin setting up tabs: lots of errors

--- App.vue
+++ App.vue
@@ -1,6 +1,28 @@
 <template>
   <div id="app">
     <h2>Boba Bill</h2>
+    <header>
+      <nav>
+        <ul>
+          <li v-for="(tab, tabName) in tabs" :key="tabName">
+            <button
+              class="tab"
+              :class="{ active: tabName === activeTab }"
+              @click="setTabActive(tabName)"
+            >
+              <span class="tab-copy">{{ tabName }}</span>
+              <!-- TODO: handle this later
+            <span class="tab-background">
+              <span class="tab-rounding left"></span>
+              <span class="tab-rounding right"></span>
+            </span> -->
+            </button>
+            <component v-bind:is="currentTabComponent" class="tab"></component>
+          </li>
+        </ul>
+      </nav>
+    </header>
+
     <FriendList v-bind:friends="friends" />
 
     <TransactionList v-bind:friends="friends" :transactions="transactions" />
@@ -19,14 +41,22 @@ import TransactionList from "./components/TransactionList.vue";
 import CalculateList from "./components/CalculateList.vue";
 
 export default {
+  el: "#app",
   components: {
     FriendList,
     TransactionList,
     CalculateList
   },
 
+  computed: {
+    currentTabComponent() {
+      return "tab-" + this.currentTab.toLowerCase();
+    }
+  },
+
   data() {
     return {
+      tabs: {},
       friends: ["Derrick", "Bunbun"],
       transactions: [
         {
